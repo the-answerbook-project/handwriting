cb4337644f8701ed5e2ed7076db3d689c9c8f7d9
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-07-05 14:32:43 +0100

    feat: rd of handwriting data

--- backend/api/router/__init__.py
+++ backend/api/router/__init__.py
@@ -1,10 +1,17 @@
+import logging
+from api.models.handwriting import Handwriting
 from fastapi import APIRouter, Depends, Request
 
-from api.dependencies import get_settings
+from api.dependencies import get_session, get_settings
 from api.schemas.status import Status
 from api.settings import Settings
+from pydantic import BaseModel
+from sqlmodel import Session, select
 
 api_router = APIRouter()
+
+logger = logging.getLogger('uvicorn.error')
+logger.setLevel(logging.DEBUG)
 
 
 @api_router.get(
@@ -35,3 +42,29 @@ def get_status(
         "environment": settings.environment,
         "testing": settings.testing,
     }
+
+@api_router.get("/handwriting")
+def handwriting(username: str, session: Session = Depends(get_session)):
+    rstmt = select(Handwriting).where(Handwriting.username == username)
+    res = session.execute(rstmt).first()
+
+    if res is None:
+        return {"error": "Handwriting not found"}
+    print("MEAP")
+    print("MEAP")
+    print("MEAP")
+    print(res[0])
+    return res[0].handwriting
+
+
+class HandwritingUpload(BaseModel):
+    username: str
+    handwriting: dict
+
+@api_router.post("/handwriting")
+def handwriting(body: HandwritingUpload, session: Session = Depends(get_session)):
+    session.add(Handwriting(username=body.username, handwriting=body.handwriting))
+    session.commit()
+    return {"success": True}
+
+    

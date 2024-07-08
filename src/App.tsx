a2c94594f0a6a7dda29476d26e2f42ead21cd91a
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2024-07-08 14:51:47 +0100

    chore: remove react-sketch-canvas
    Co-authored-by: Matthew Alex <matalex412@gmail.com>
    Co-authored-by: nick-bolas <nick-bolas@users.noreply.github.com>
    Co-authored-by: Kishan <kishansambhi@hotmail.co.uk>

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { Excalidraw, MainMenu, Sidebar, serializeAsJSON } from '@excalidraw/excalidraw'
-import { loadFromJSON } from '@excalidraw/excalidraw/types/data'
-import { ExcalidrawElement } from '@excalidraw/excalidraw/types/element/types'
+import { Excalidraw, MainMenu, serializeAsJSON } from '@excalidraw/excalidraw'
 import {
-  AppState,
-  BinaryFiles,
   ExcalidrawImperativeAPI,
   ExcalidrawInitialDataState,
 } from '@excalidraw/excalidraw/types/types'
 import { Button, Theme } from '@radix-ui/themes'
 import { MathJax, MathJaxContext } from 'better-react-mathjax'
-import { useCallback, useEffect, useRef, useState } from 'react'
-import { ReactSketchCanvas, ReactSketchCanvasRef } from 'react-sketch-canvas'
+import { useCallback, useEffect, useState } from 'react'
 
 import './App.css'
 import UserSelector from './UserSelector'
@@ -22,11 +17,8 @@ const USER = 'hpotter'
 function App() {
   const [latex, setLatex] = useState('\\(\\frac{10}{4x} \\approx 2^{12}\\)')
   const [username, setUsername] = useState(USER)
-  const [eraseMode, setEraseMode] = useState(false)
   const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI | null>(null)
   const [excalidrawData, setExcalidrawData] = useState<ExcalidrawInitialDataState | null>(null)
-
-  const sketchRef = useRef<ReactSketchCanvasRef>(null)
 
   const exportSVG = () => {
     const asJSON = serializeAsJSON(
@@ -41,13 +33,6 @@ function App() {
     })
   }
 
-  const toggleEraseMode = () => {
-    const newEraseMode = !eraseMode
-    // @ts-expect-error: sketchRef is not null
-    sketchRef.current.eraseMode(newEraseMode)
-    setEraseMode(newEraseMode)
-  }
-
   const renderLatex = useCallback(() => {
     // api call
     axiosInstance.get(`/${username}/latex`).then((res) => {
@@ -58,27 +43,17 @@ function App() {
   useEffect(() => {
     axiosInstance.get(`/${username}/handwriting`).then((res) => {
       setExcalidrawData(res.data.excalidraw)
-      console.log(res.data.excalidraw)
       if (!res.data.error) {
         excalidrawAPI?.updateScene({
           appState: res.data.excalidraw.appState,
           elements: res.data.excalidraw.elements,
         })
+        // renderLatex()
       } else {
         excalidrawAPI?.updateScene({ elements: [] })
       }
-      //loadFromJSON(res.data.excalidraw.appState, res.data.excalidraw.elements)
-
-      // sketchRef.current?.clearCanvas()
-
-      // if (!res.data.error) {
-      //   sketchRef.current?.loadPaths(res.data)
-      //   renderLatex()
-      // }
     })
   }, [username, renderLatex, excalidrawAPI])
-
-  // setExcalidrawState({ elements, appState, files, type: "local" })
 
   return (
     <Theme radius="small" appearance="dark">
@@ -98,9 +73,6 @@ function App() {
           <MathJax>{latex}</MathJax>
           <Button onClick={renderLatex}>Render Latex 🔎</Button>
         </div>
-        <br />
-        <Button onClick={toggleEraseMode}>{eraseMode ? 'Use Pen ✏️' : 'Use Eraser 🧼'}</Button>
-        <br />
         <br />
         <Button onClick={exportSVG}>Save 💾</Button>
         <UserSelector username={username} setUsername={setUsername} />

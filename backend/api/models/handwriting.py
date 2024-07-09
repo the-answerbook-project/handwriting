2ef7a758e05930ac0c27ce157d194df98e9400bc
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-07-09 11:20:40 +0100

    feat: store latex in DB on save

--- backend/api/models/handwriting.py
+++ backend/api/models/handwriting.py
@@ -1,11 +1,10 @@
-from typing import Dict, Optional
+from typing import Optional
 from sqlalchemy import Column
-import sqlmodel
 from sqlalchemy.dialects.postgresql import JSONB
-from sqlmodel import JSON, Field, SQLModel
+from sqlmodel import Field, SQLModel
 
 
 class Handwriting(SQLModel, table=True):
     username: str = Field(primary_key=True)
     handwriting: Optional[dict] = Field(sa_column=Column(JSONB), default={})
-        
+    latex: Optional[str] = Field(sa_column=Column(JSONB), default="")
--- backend/api/router/__init__.py
+++ backend/api/router/__init__.py
@@ -51,22 +51,34 @@ def handwriting(username: str, session: Session = Depends(get_session)):
 
     if res is None:
         return {"error": "Handwriting not found"}
-    print(res[0])
     return res[0].handwriting
+
 
 class HandwritingUpload(BaseModel):
     handwriting: dict
 
+
 @api_router.put("/{username}/handwriting")
-def handwriting(username: str, body: HandwritingUpload, session: Session = Depends(get_session)):
+def put_handwriting(
+    username: str,
+    body: HandwritingUpload,
+    session: Session = Depends(get_session)
+):
     existing_handwriting = session.get(Handwriting, username)
     if existing_handwriting:
         existing_handwriting.handwriting = body.handwriting
     else:
-        new_handwriting = Handwriting(username=username, handwriting=body.handwriting)
-        session.add(new_handwriting)
+        existing_handwriting = Handwriting(username=username,
+                                           handwriting=body.handwriting)
+        session.add(existing_handwriting)
+
+    # Update LaTeX
+    latex = react_canvas_to_mathpix_strokes(existing_handwriting.handwriting)
+    existing_handwriting.latex = latex
+
     session.commit()
     return {"success": True}
+
 
 @api_router.get("/{username}/latex")
 def latex(username: str, session: Session = Depends(get_session)):
@@ -75,11 +87,7 @@ def latex(username: str, session: Session = Depends(get_session)):
 
     if res is None:
         return {"error": "Handwriting not found"}
-    
+
     handwritingraw = res[0]
 
-    return react_canvas_to_mathpix_strokes(handwritingraw.handwriting)
-
-
-
-    
+    return handwritingraw.latex  
